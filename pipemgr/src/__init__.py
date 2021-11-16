ac60944349013a40b080fdd85c2ab30af1e0090c
m1yag1 <mike.arbelaez@rice.edu>
2021-11-16 17:19:47 -0600

    Cleaned up some formatting. Might want to consider flake8 linting ;)

--- pipemgr/src/__init__.py
+++ pipemgr/src/__init__.py
@@ -7,8 +7,10 @@ WORKING_ROOT = MODULE_ROOT.parent
 OUTPUT_ROOT = WORKING_ROOT/"out"
 TEMPLATE_ROOT = MODULE_ROOT/"templates"
 
+
 def load_yaml(yaml_str: str):
     return yaml.load(yaml_str, Loader=yaml.SafeLoader)
+
 
 if not OUTPUT_ROOT.exists():
     OUTPUT_ROOT.mkdir()
